006ebd448c28941a541ebe76b2f4905c19347432
surendra-Titus <nagasurendra66@gmail.com>
2025-04-10 15:46:05 +0530

    Unused code.

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -2,11 +2,8 @@ import { Outlet } from "react-router-dom";
 import { Flex, Box } from "@chakra-ui/react";
 import Header from "./Header";
 import Sidemenu from "./Sidemenu";
-import { useTranslation } from "react-i18next";
 
 const Body = () => {
-  const { i18n } = useTranslation();
-
   return (
     <Box height="100vh" width="100vw" overflow={"hidden"}>
       <Header />
